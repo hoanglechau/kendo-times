c50643ee02b2c24aa04846142240114204c2fa61
hoanglechau <hoangchau612@gmail.com>
2022-09-01 22:15:22 +0700

    Update history.js

--- scripts/history.js
+++ scripts/history.js
@@ -3,7 +3,6 @@
 /* -------------------------------------------------------------------------- */
 
 new fullpage('#fullpage', {
-  //options here
   autoScrolling: true,
   scrollHorizontally: true,
 });

fde06aa45bef460867c7660202b6c1a93408ee28
hoanglechau <hoangchau612@gmail.com>
2022-09-05 14:53:16 +0700

    Update gear.js

--- scripts/gear.js
+++ scripts/gear.js
@@ -2,7 +2,11 @@
 /*                                  Libraries                                 */
 /* -------------------------------------------------------------------------- */
 
+// The code here was adapted and edited from the documentation of the "AOS" JavaScript library in order to set up the fade-in scrolling effect
+
 AOS.init();
+
+// Adopted and edited code ends here. All the code from this point on was written by me
 
 /* -------------------------------------------------------------------------- */
 /*                                  Selectors                                 */
@@ -21,6 +25,7 @@ const imageList = document.querySelectorAll('.gear__image');
 /*                                  Functions                                 */
 /* -------------------------------------------------------------------------- */
 
+// This function lets users close the modal by hitting the "Escape" key on the keyboard
 function handlePressEscape(evt) {
   if (evt.key === 'Escape') {
     closeModal(document.querySelector('.modal_open'));
@@ -43,8 +48,10 @@ function closeModal(modal) {
 
 imageList.forEach((imageEl) => {
   imageEl.addEventListener('click', function () {
+    // Set the src and alt attributes of the image in the modal to be those of the image that the users clicked on
     previewImage.src = imageEl.src;
     previewImage.alt = imageEl.alt;
+    // Set the preview image caption to be the same as the alt text of the image that was clicked on
     previewImageTitle.textContent = imageEl.alt;
     openModal(previewModal);
   });
@@ -54,6 +61,7 @@ previewModalCloseButton.addEventListener('click', () =>
   closeModal(previewModal),
 );
 
+// This allows the users to close the modal by clicking outside the modal
 previewModal.addEventListener('mousedown', (evt) => {
   if (evt.target === previewModal) {
     closeModal(previewModal);

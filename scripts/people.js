63747b6e17df42fcd3522b20add380cab9d6e13e
hoanglechau <hoangchau612@gmail.com>
2022-09-05 14:53:10 +0700

    Update people.js

--- scripts/people.js
+++ scripts/people.js
@@ -2,10 +2,14 @@
 /*                                  Libraries                                 */
 /* -------------------------------------------------------------------------- */
 
+// The code here was adapted and edited from the documentation of the "fullPage.js" JavaScript library in order to set up the fullscreen scrolling effect
+
 new fullpage('#fullpage', {
   autoScrolling: true,
   scrollHorizontally: true,
 });
+
+// Adopted and edited code ends here. All the code from this point on was written by me
 
 /* -------------------------------------------------------------------------- */
 /*                                  Selectors                                 */
@@ -24,6 +28,7 @@ const imageList = document.querySelectorAll('.people__image');
 /*                                  Functions                                 */
 /* -------------------------------------------------------------------------- */
 
+// This function lets users close the modal by hitting the "Escape" key on the keyboard
 function handlePressEscape(evt) {
   if (evt.key === 'Escape') {
     closeModal(document.querySelector('.modal_open'));
@@ -46,8 +51,10 @@ function closeModal(modal) {
 
 imageList.forEach((imageEl) => {
   imageEl.addEventListener('click', function () {
+    // Set the src and alt attributes of the image in the modal to be those of the image that the users clicked on
     previewImage.src = imageEl.src;
     previewImage.alt = imageEl.alt;
+    // Set the preview image caption to be the same as the alt text of the image that was clicked on
     previewImageTitle.textContent = imageEl.alt;
     openModal(previewModal);
   });
@@ -57,6 +64,7 @@ previewModalCloseButton.addEventListener('click', () =>
   closeModal(previewModal),
 );
 
+// This allows the users to close the modal by clicking outside the modal
 previewModal.addEventListener('mousedown', (evt) => {
   if (evt.target === previewModal) {
     closeModal(previewModal);

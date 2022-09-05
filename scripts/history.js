10594efa730f5cce06627e07365178cee2386fb3
hoanglechau <hoangchau612@gmail.com>
2022-09-05 14:53:14 +0700

    Update history.js

--- scripts/history.js
+++ scripts/history.js
@@ -2,6 +2,8 @@
 /*                                  Libraries                                 */
 /* -------------------------------------------------------------------------- */
 
+// The code here was adapted and edited from the documentation of the "fullPage.js" JavaScript library
+
 new fullpage('#fullpage', {
   autoScrolling: true,
   scrollHorizontally: true,
